456f4622eb6831dcce831037f60f266173944511
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-14 19:06:04 +0200

    Bump AVA version

--- test.js
+++ test.js
@@ -1,23 +1,18 @@
 import fs from 'fs';
+import path from 'path';
 import csvParser from 'csv-parser';
 import test from 'ava';
-import processFile from './';
+import fn from './';
 
 test('process file using filepath', async t => {
-	t.plan(2);
-
-	const data = await processFile(__dirname + '/fixture.csv', csvParser());
-
+	const data = await fn(path.join(__dirname, 'fixture.csv'), csvParser());
 	t.is(data[0].foo, 'unicorn');
 	t.is(data[0].bar, 'cat');
 });
 
 test('process file using buffer', async t => {
-	t.plan(2);
-
-	const buf = fs.readFileSync(__dirname + '/fixture.csv');
-	const data = await processFile(buf, csvParser());
-
+	const buf = fs.readFileSync(path.join(__dirname, 'fixture.csv'));
+	const data = await fn(buf, csvParser());
 	t.is(data[0].foo, 'unicorn');
 	t.is(data[0].bar, 'cat');
 });
